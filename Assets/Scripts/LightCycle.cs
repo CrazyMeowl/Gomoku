ad06e71cb5cde801ba249fdd11b24ef86c714ee3
Tran Cao Bao Phuc <62349601+CrazyMeowl@users.noreply.github.com>
2023-10-29 14:09:16 +0700

    + Board Size Adjust, Light Cycle

---
+++ Assets/Scripts/LightCycle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCycle : MonoBehaviour
+{
+    float a = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        a += 0.1f;
+        transform.rotation = Quaternion.Euler(new Vector3(70, a, 0));
+
+    }
+}
